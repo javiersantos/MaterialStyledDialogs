50219ffd4d3f9f5ea4287b0a496cc8f9c85d453f
Lam Fung <chkfung93@gmail.com>
2018-03-19 15:31:42 +0800

    Add method to set custom header icon animation.

--- library/src/main/java/com/github/javiersantos/materialstyleddialogs/UtilsAnimation.java
+++ library/src/main/java/com/github/javiersantos/materialstyleddialogs/UtilsAnimation.java
@@ -1,30 +1,15 @@
 package com.github.javiersantos.materialstyleddialogs;
 
 import android.content.Context;
+import android.support.annotation.AnimRes;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 class UtilsAnimation {
 
-    static void zoomInAndOutAnimation(Context context, final ImageView image) {
-        Animation zoomInAnimation = AnimationUtils.loadAnimation(context, R.anim.md_styled_zoom_in);
-        final Animation zoomOutAnimation = AnimationUtils.loadAnimation(context, R.anim.md_styled_zoom_out);
-
-        zoomInAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {}
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                image.startAnimation(zoomOutAnimation);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {}
-        });
-
-        image.startAnimation(zoomInAnimation);
+    static void startAnimation(Context context, ImageView image, @AnimRes int anim){
+        Animation animation = AnimationUtils.loadAnimation(context,anim);
+        image.startAnimation(animation);
     }
-
 }
